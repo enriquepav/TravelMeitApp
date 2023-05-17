ebc06ab9245686ad5c4749fb369444539ceaab58
Enrique Alata <e.alatavences@gmail.com>
2023-05-17 08:20:12 -0500

    Initial Commit

---
+++ TravelMeitApp/TravelMeitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMeitAppApp.swift
+//  TravelMeitApp
+//
+//  Created by MAC1DIGITAL20 on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelMeitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
