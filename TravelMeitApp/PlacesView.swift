3d0b281ecc9dce5ffe1e2d2bd86f37a9620cf6a8
Enrique Alata <e.alatavences@gmail.com>
2023-05-22 16:50:53 -0500

    Create a new view "Places"

--- TravelMeitApp/PlacesView.swift
+++ TravelMeitApp/PlacesView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct PlacesView: View {
+    
+    @State private var isChecked: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("barrancoImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .imageScale(.large)
+                        .frame(width: 100, height: 100)
+            VStack {
+                Text("Plaza de barranco")
+                Spacer()
+                HStack {
+                    HStack {
+                        Image(systemName: "globe")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .imageScale(.large)
+                                    .frame(width: 10, height: 10)
+                        Text("0.15 KM")
+                        
+                    }
+                    Spacer()
+                    Button(action: {
+                                isChecked.toggle()
+                            }) {
+                                HStack {
+                                    Image(systemName: isChecked ? "checkmark.square.fill" : "square")
+                                        .foregroundColor(isChecked ? .blue : .gray)
+                                        .imageScale(.large)
+                                    
+                                    Text("")
+                                }
+                            }
+                    
+                }
+            }
+        }
+        
     }
 }
 
