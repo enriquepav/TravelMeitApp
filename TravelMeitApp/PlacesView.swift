45c84fb8faabcf1958146404238c1aa8bf40d4b1
Enrique Alata <e.alatavences@gmail.com>
2023-05-25 11:29:03 -0500

    modify Places view

--- TravelMeitApp/PlacesView.swift
+++ TravelMeitApp/PlacesView.swift
@@ -12,41 +12,46 @@ struct PlacesView: View {
     @State private var isChecked: Bool = false
     
     var body: some View {
-        ZStack {
-            Image("barrancoImage")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .imageScale(.large)
-                        .frame(width: 100, height: 100)
-            VStack {
-                Text("Plaza de barranco")
-                Spacer()
-                HStack {
+        VStack {
+            ZStack {
+                Image("barrancoImage")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .imageScale(.large)
+                            .frame(width: 300, height: 300)
+                VStack {
+                    Text("Plaza de barranco")
+                    Spacer()
                     HStack {
-                        Image(systemName: "globe")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .imageScale(.large)
-                                    .frame(width: 10, height: 10)
-                        Text("0.15 KM")
+                        HStack {
+                            Image(systemName: "globe")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .imageScale(.large)
+                                        .frame(width: 10, height: 10)
+                            Text("0.15 KM")
+                            
+                        }
+                        Spacer()
+                        Button(action: {
+                                    isChecked.toggle()
+                                }) {
+                                    HStack {
+                                        Image(systemName: isChecked ? "checkmark.square.fill" : "square")
+                                            .foregroundColor(isChecked ? .blue : .gray)
+                                            .imageScale(.large)
+                                        
+                                        Text("")
+                                    }
+                                }
                         
                     }
-                    Spacer()
-                    Button(action: {
-                                isChecked.toggle()
-                            }) {
-                                HStack {
-                                    Image(systemName: isChecked ? "checkmark.square.fill" : "square")
-                                        .foregroundColor(isChecked ? .blue : .gray)
-                                        .imageScale(.large)
-                                    
-                                    Text("")
-                                }
-                            }
-                    
                 }
             }
         }
+        .frame(width: 200, height: 300)
+        
+
         
     }
 }
