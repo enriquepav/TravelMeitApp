94f8a18aebf4f8c138a81064e2f607f556e3f806
Enrique Alata <e.alatavences@gmail.com>
2023-05-30 08:17:09 -0500

    change principalview

--- TravelMeitApp/Views/ContentView.swift
+++ TravelMeitApp/Views/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                Image("CiudadDifuminada")
+                Image("PantallaEntrada")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .edgesIgnoringSafeArea(.all)
