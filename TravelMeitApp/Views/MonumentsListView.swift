e23bcb070ca48669c586d0ea5edc68a98ea2d922
Enrique Alata <e.alatavences@gmail.com>
2023-05-31 07:52:03 -0500

    Code clean

--- TravelMeitApp/Views/MonumentsListView.swift
+++ TravelMeitApp/Views/MonumentsListView.swift
@@ -10,10 +10,8 @@ import CoreLocation
 
 struct MonumentsListView: View {
     
-    let items: [Float] = [1.50, 0.15, 0.89, 2.35, 1.69, 3.45]// Reemplaza "YourDataType" con el tipo de dato que utilices en tu grid
-    
     @State private var selectedOption: Int = 1 // Estado de selección
-    let distanceSelected: [Float] = [0.15, 0.26, 50.00]
+    let distanceSelected: [Float] = [0.15, 0.26, 50.00] // Cambiar distancias
     let options = ["ic_3km", "ic_10km", "ic_50km"]
     let optionSelected = ["ic_3kmSelected", "ic_10kmSelected", "ic_50kmSelected"]
     
@@ -31,15 +29,7 @@ struct MonumentsListView: View {
                         NavigationLink(destination: MonumentDetailView(image: "barrancoframe", name: item.monument, distance: Float(viewModel.calculateDistance(point1: viewModel.coordinateZero, point2: CLLocationCoordinate2D(latitude: item.Lattitude ?? 0.00, longitude: item.Longitude ?? 0.00))))) {
                             MonumentCelView(monumentImage: "barrancoframe", distance: Float(viewModel.calculateDistance(point1: viewModel.coordinateZero, point2: CLLocationCoordinate2D(latitude: item.Lattitude ?? 0.00, longitude: item.Longitude ?? 0.00))), title: item.monument)
                         }
-                        //                            .navigationBarItems(trailing:
-                        //                                                    Button(action: {
-                        //                                                        // Acción del botón
-                        //                                                    }) {
-                        //                                                        Image(systemName: "ellipsis")
-                        //                                                            .rotationEffect(Angle(degrees: 90))
-                        //                                                    }
-                        //                                                )
-                        
+ 
                     }
                 }
                 .padding()
@@ -69,21 +59,6 @@ struct MonumentsListView: View {
                         }
                     }
                 }
-            
-//                HStack(alignment: .center) {
-//                    Image("logoTravelmeit")
-//                        .resizable()
-//                        .frame(width: 150, height: 40)
-//                    Spacer(minLength: 50)
-//                    Image("ic_3km")
-//                        .resizable()
-//                        .frame(width: 32, height: 32)
-//                    Image("ic_10km")
-//                        .resizable()
-//                        .frame(width: 32, height: 32)
-//                    Image("ic_50km")
-//                        .resizable()
-//                        .frame(width: 32, height: 32)
             }
             
         }
