703fbf33731e864c96356395a795b0530e5f44a2
Kath Caillahua <maria.caillahua@naranjax.com>
2023-06-02 13:55:46 -0500

    fix sign up design

--- TravelMeitApp/UI/Views/CreateAccountView.swift
+++ TravelMeitApp/UI/Views/CreateAccountView.swift
@@ -22,177 +22,155 @@ struct CreateAccountView: View {
     @State private var selectedOption: Int = 0 // Estado de selección
     let options = ["Aventurero", "Turista", "Gastronómico", "Mochilero", "De lujo"]
     
-
+    
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Group {
-                    VStack {
-                        Text("Crea tu cuenta")
-                        TextField("Nombre", text: $name)
-                            .padding()
-                            .frame(maxWidth: 350)
-                            .background(Color.gray.opacity(0.09))
-                            .cornerRadius(150)
-                            .padding()
-                            .font(.body)
-                            .shadow(color: Color.black.opacity(0.9), radius: 5, x: 10, y: 10)
-                        
-                        Picker("", selection: $selectedCountry,
-                               content: {
-                            Text("País de origen:")
-                            ForEach(countrys, id: \.self) { index in
-                                Text(index)
-                                    .tag(index)
-                            }
-                        }
-                               
-                        )
-                        .padding()
-                        .frame(maxWidth: 350)
-                        .background(Color.gray.opacity(0.09))
-                        .cornerRadius(150)
-                        .font(.body)
-                        .shadow(color: Color.black.opacity(0.9), radius: 5, x: 10, y: 10)
-                        
-                        Picker("", selection: $selectedLanguage,
-                               content: {
-                            Text("Idioma:")
-                            ForEach(languages, id: \.self) { index in
-                                Text(index)
-                                    .tag(index)
-                            }
-                        }
-                               
-                        )
-                        .padding()
-                        .frame(maxWidth: 350)
-                        .background(Color.gray.opacity(0.09))
-                        .cornerRadius(150)
-                        .font(.body)
-                        .shadow(color: Color.black.opacity(0.9), radius: 5, x: 10, y: 10)
-                        
-                        DatePicker("Fecha de nacimiento - dd/mm/aaaa", selection: $selectedDate, displayedComponents: .date)
-                            .datePickerStyle(.compact) // Estilo del DatePicker
-                            .padding()
-                            .frame(maxWidth: 350)
-                            .background(Color.gray.opacity(0.09))
-                            .cornerRadius(150)
-                            .font(.body)
-                            .shadow(color: Color.black.opacity(0.9), radius: 5, x: 10, y: 10)
-                        
-                        TextField("Correo", text: $mail)
-                            .padding()
-                            .frame(maxWidth: 350)
-                            .background(Color.gray.opacity(0.09))
-                            .cornerRadius(150)
-                            .padding()
-                            .font(.body)
-                            .shadow(color: Color.black.opacity(0.9), radius: 5, x: 10, y: 10)
-                        
-                        TextField("Número y/o correo de emergencia", text: $emergencyData)
-                            .padding()
-                            .frame(maxWidth: 350)
-                            .background(Color.gray.opacity(0.09))
-                            .cornerRadius(150)
-                            .padding()
-                            .font(.body)
-                            .shadow(color: Color.black.opacity(0.9), radius: 5, x: 10, y: 10)
-                        
-                        Picker("", selection: $sizeType,
-                               content: {
-                            Text("Fijar medida de distancia")
-                            ForEach(sizes, id: \.self) { index in
-                                Text(index)
-                                    .tag(index)
-                            }
-                        }
-                               
-                        )
-                        .padding()
-                        .frame(maxWidth: 350)
-                        .background(Color.gray.opacity(0.09))
-                        .cornerRadius(150)
-                        .font(.body)
-                        .shadow(color: Color.black.opacity(0.9), radius: 5, x: 10, y: 10)
-                        
-                    }
-                }
+        ZStack{
+            Image("bg_start")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .blur(radius: 3)
+            
+            VStack{
+                Image("logoTravelmeit")
+                    .renderingMode(.template)
+                    .foregroundColor(.white)
+                    .frame(width: 200, height: 50)
+                    .padding()
                 
-                
-                Group {
+                ScrollView {
                     
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack(spacing: 0) {
-                            ForEach(0..<options.count, id: \.self) { index in
-                                Button(action: {
-                                    selectedOption = index // Actualizar el estado de selección al hacer clic en la opción
-                                }) {
-                                    Text(options[index])
-                                        .font(.title)
-                                        .padding()
-                                        .background(selectedOption == index ? Color.blue.opacity(0.5) : Color.clear) // Marcar la opción seleccionada
-                                        .cornerRadius(10)
+                    VStack {
+                        
+                        VStack {
+                            Text("Create your account").font(.title)                            .foregroundColor(.principalColor).padding(10)
+                            TextField("Name", text: $name)
+                                .padding()
+                                .frame(maxWidth: 350)
+                                .background(Color.white.opacity(0.7))
+                                .cornerRadius(150)
+                                .font(.body)
+                                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                            
+                            HStack{
+                                Text("Country of origin").foregroundColor(Color.gray)
+                                Spacer()
+                                Picker("", selection: $selectedCountry,
+                                       content: {
+                                    ForEach(countrys, id: \.self) { index in
+                                        Text(index).tag(index)
+                                    }
+                                }).pickerStyle(.menu)
+                            }
+                            .padding()
+                            .frame(maxWidth: 350)
+                            .background(Color.white.opacity(0.7))
+                            .cornerRadius(150)
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                            
+                            HStack{
+                                Text("Language:").foregroundColor(Color.gray)
+                                Spacer()
+                                Picker("", selection: $selectedLanguage,
+                                       content: {
+                                    ForEach(languages, id: \.self) { index in
+                                        Text(index)
+                                            .tag(index)
+                                    }
+                                }).pickerStyle(.menu)
+                            }
+                            .padding()
+                            .frame(maxWidth: 350)
+                            .background(Color.white.opacity(0.7))
+                            .cornerRadius(150)
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                            
+                            
+                            DatePicker("Date of birth -\n dd/mm/aaaa", selection: $selectedDate, displayedComponents: .date)
+                                .padding()
+                                .frame(maxWidth: 350)
+                                .background(Color.white.opacity(0.7))
+                                .cornerRadius(150)
+                                .font(.body)
+                                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                                .foregroundColor(Color.gray)
+                            
+                            TextField("Email", text: $mail)
+                                .padding()
+                                .frame(maxWidth: 350)
+                                .background(Color.white.opacity(0.7))
+                                .cornerRadius(150)
+                                .font(.body)
+                                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                            
+                            TextField("Emergency number and/or email", text: $emergencyData)
+                                .padding()
+                                .frame(maxWidth: 350)
+                                .background(Color.white.opacity(0.7))
+                                .cornerRadius(150)
+                                .font(.body)
+                                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                            
+                            HStack{
+                                Text("Set distance measurement:").foregroundColor(Color.gray)
+                                Picker("", selection: $sizeType,
+                                       content: {
+                                    ForEach(sizes, id: \.self) { index in
+                                        Text(index)
+                                            .tag(index)
+                                    }
+                                }).pickerStyle(.menu)
+                            }
+                            .padding()
+                            .frame(maxWidth: 350)
+                            .background(Color.white.opacity(0.7))
+                            .cornerRadius(150)
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                            
+                            Text("Type of traveler").font(.system(size: 25))                            .foregroundColor(.principalColor).padding(8)
+                            Text("Select one or more options").foregroundColor(Color.gray).font(.system(size: 15))
+                            
+                        }
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            LazyHStack(spacing: 0) {
+                                ForEach(0..<options.count, id: \.self) { index in
+                                    Button(action: {
+                                        selectedOption = index // Actualizar el estado de selección al hacer clic en la opción
+                                    }) {
+                                        Text(options[index])
+                                            .font(.title)
+                                            .padding()
+                                            .background(selectedOption == index ? Color.blue.opacity(0.5) : Color.clear) // Marcar la opción seleccionada
+                                            .cornerRadius(10)
+                                    }
                                 }
                             }
+                        }.padding(20)
+                        
+                        Button(action: {
+                            // Acción a realizar cuando se presione el botón
+                        }) {
+                            Text("Continue")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.thirdColor)
+                                .cornerRadius(30)
                         }
+                        
                     }
-                    
-                }
-                Button(action: {
-                          // Acción a realizar cuando se presione el botón
-                      }) {
-                          Text("Continuar")
-                              .foregroundColor(.white)
-                              .padding()
-                              .background(Color.orange)
-                              .cornerRadius(10)
-                      }
-                HStack {
-                            Button(action: {
-                                // Acción a realizar cuando se presione el botón de Facebook
-                            }) {
-                                Image(systemName: "globe") // Reemplaza "facebook_icon" con el nombre de tu imagen de Facebook
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 30, height: 30)
-                            }
-                            
-                            Button(action: {
-                                // Acción a realizar cuando se presione el botón de Instagram
-                            }) {
-                                Image(systemName: "globe") // Reemplaza "instagram_icon" con el nombre de tu imagen de Instagram
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 30, height: 30)
-                            }
-                    
-                            Button(action: {
-                                // Acción a realizar cuando se presione el botón de Facebook
-                            }) {
-                                Image(systemName: "globe") // Reemplaza "facebook_icon" con el nombre de tu imagen de Facebook
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 30, height: 30)
-                            }
-                            
-                            Button(action: {
-                                // Acción a realizar cuando se presione el botón de Instagram
-                            }) {
-                                Image(systemName: "globe") // Reemplaza "instagram_icon" con el nombre de tu imagen de Instagram
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 30, height: 30)
-                            }
-            
-                    }
+                }.background(Color.white.opacity(0.7)).cornerRadius(20).padding(EdgeInsets(top: 20, leading: 80, bottom: 10, trailing: 80))
                 
             }
+            
+            
         }
+        
+        
     }
     
-   
+    
 }
 
 
