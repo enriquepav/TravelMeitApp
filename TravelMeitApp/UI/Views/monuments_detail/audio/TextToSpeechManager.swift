5d73ada046c832f4895aef51e68865b7408b1c46
Kath Caillahua <maria.caillahua@naranjax.com>
2023-06-28 17:02:58 -0500

    Add audio config

---
+++ TravelMeitApp/UI/Views/monuments_detail/audio/TextToSpeechManager.swift
@@ -0,0 +1,44 @@
+//
+//  TextToSpeechManager.swift
+//  TravelMeitApp
+//
+//  Created by Katherine Caillahua Castillo on 28/06/23.
+//
+
+import Foundation
+import AVFoundation
+
+class TextToSpeechManager: NSObject, ObservableObject, AVSpeechSynthesizerDelegate {
+    let speechSynthesizer = AVSpeechSynthesizer()
+
+    override init() {
+        super.init()
+        speechSynthesizer.delegate = self
+        // Configura la sesión de audio para reproducir en segundo plano
+        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        try? AVAudioSession.sharedInstance().setActive(true)
+    }
+
+    func speakText(_ text: String) {
+        let speechUtterance = AVSpeechUtterance(string: text)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US") // Establece el idioma de la voz
+        speechSynthesizer.speak(speechUtterance)
+    }
+
+    func pauseSpeech() {
+        speechSynthesizer.pauseSpeaking(at: .immediate)
+    }
+
+    func continueSpeech() {
+        speechSynthesizer.continueSpeaking()
+    }
+
+    func stopSpeech() {
+        speechSynthesizer.stopSpeaking(at: .immediate)
+    }
+
+    // AVSpeechSynthesizerDelegate
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        // La lectura del texto ha finalizado
+    }
+}
